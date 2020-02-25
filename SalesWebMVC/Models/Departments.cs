d7f509fdc12ac6e34f5aa70293bb28b3784dd823
Michell Polastrelli <michelkronus@hotmail.com>
2020-02-24 23:19:43 -0300

    Outras entidades

--- SalesWebMVC/Models/Departments.cs
+++ SalesWebMVC/Models/Departments.cs
@@ -9,5 +9,28 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Departments()
+        {
+
+        }
+
+        public Departments(int id, string name, ICollection<Seller> sellers)
+        {
+            Id = id;
+            Name = name;
+            Sellers = sellers;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
