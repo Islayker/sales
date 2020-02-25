0b5fa0d4f12b96923fdcd162a01811d2a37cba76
Michell Polastrelli <michelkronus@hotmail.com>
2020-02-24 21:41:55 -0300

    Inclusao Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departments> depart = new List<Departments>();
+            depart.Add(new Departments { Id = 1, Name = "Eletronics" });
+            depart.Add(new Departments { Id = 2, Name = "Fashion" });
+
+            return View(depart);
+        }
+    }
+}
